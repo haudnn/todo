d76d1bc9fdd893e469a0c6844cf14e583cb5aa41
S3C0ND19 <90059594+S3C0ND19@users.noreply.github.com>
2022-01-14 20:24:18 +0700

    first commit

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,19 +1,46 @@
 import { Col, Row, Input, Button, Select, Tag } from 'antd';
 import Todo from '../Todo';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useRef } from 'react'
+import { addTodo } from '../../redux/actions'
+import { v4 as uuidv4 } from 'uuid'
+import { todoListSelector } from '../../redux/selectors';
 
 export default function TodoList() {
+  const [todoName,setTodoName] = useState('')
+  const [prioriry,setPriority] = useState('Medium')
+  const todoList = useSelector(todoListSelector)
+  const refTodoName = useRef()
+  console.log(todoList)
+  const dispatch = useDispatch()  
+  const handleAdd = () => {
+    dispatch(addTodo({
+      id:uuidv4(),
+      name:todoName,
+      prioriry:prioriry,
+      completed:false
+    }))
+    setTodoName('')
+    refTodoName.current.focus()
+  }
+  const handleTodoName = (e) => {
+    setTodoName(e.target.value)
+  }
+  const handlePriority = (value) => {
+    setPriority(value)
+  }
   return (
     <Row style={{ height: 'calc(100% - 40px)' }}>
       <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
-        <Todo name='Learn React' prioriry='High' />
-        <Todo name='Learn Redux' prioriry='Medium' />
-        <Todo name='Learn JavaScript' prioriry='Low' />
+        {todoList.map(todo => 
+          <Todo key={todo.id} name={todo.name} prioriry={todo.prioriry} />
+        )}
       </Col>
       <Col span={24}>
-        <Input.Group style={{ display: 'flex' }} compact>
-          <Input />
-          <Select defaultValue="Medium">
-            <Select.Option value='High' label='High'>
+        <Input.Group  style={{ display: 'flex' }} compact >
+          <Input ref={refTodoName} value={todoName} onChange={handleTodoName} />
+          <Select defaultValue="Medium" value={prioriry} onChange={handlePriority}>
+          <Select.Option value='High' label='High'>
               <Tag color='red'>High</Tag>
             </Select.Option>
             <Select.Option value='Medium' label='Medium'>
@@ -23,7 +50,7 @@ export default function TodoList() {
               <Tag color='gray'>Low</Tag>
             </Select.Option>
           </Select>
-          <Button type='primary'>
+          <Button type='primary' onClick={handleAdd}>
             Add
           </Button>
         </Input.Group>
